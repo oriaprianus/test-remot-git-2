fd71b6a87af7d4fab8db76212b25f18e23fde21c
oriaprianus <oriaprianus@gmail.com>
2022-08-06 16:13:54 +0700

    menambahkan paragrapf

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remot</title>
+</head>
+
+<body>
+    <p>Hello World!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ipsum iusto saepe, rerum odio reiciendis est nisi, repudiandae laborum veritatis recusandae assumenda. Corrupti debitis dignissimos delectus! Quisquam sunt corporis alias.</p>
+</body>
+
+</html>
